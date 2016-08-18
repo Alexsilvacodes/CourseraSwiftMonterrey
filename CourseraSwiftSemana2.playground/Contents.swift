55ddc2fc96f63caea68813e4cc8b0efce01fd946
Alexsays <h4ll0ck@gmail.com>
2016-08-18 13:34:02 +0100

    Semana 2

---
+++ CourseraSwiftSemana2.playground/Contents.swift
@@ -0,0 +1,18 @@
+// Coursera
+
+import UIKit
+
+for i in 0...100 {
+    if i % 5 == 0 {
+        print("\(i) Bingo!!!")
+    }
+    if i % 2 == 0 {
+        print("\(i) par!!!")
+    }
+    if i % 2 != 0 {
+        print("\(i) impar!!!")
+    }
+    if i > 30 && i < 40 {
+        print("\(i) Viva Swift!!!")
+    }
+}
